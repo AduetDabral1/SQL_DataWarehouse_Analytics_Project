07d844121ed45dd2ea86d3136ec4d3b5b3cf5614
Aduet Dabral <dabral.aduet@gmail.com>
2025-10-16 15:45:18 +0530

    Add files via upload

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -14,34 +14,46 @@ Usage Notes:
 ===============================================================================
 */
 
--- ====================================================================
--- Checking 'gold.dim_customers'
--- ====================================================================
+USE DataWarehouse;
+GO
+
+/*
+====================================================================
+Checking 'gold.dim_customers'
+====================================================================
+*/
+
 -- Check for Uniqueness of Customer Key in gold.dim_customers
 -- Expectation: No results 
-SELECT 
-    customer_key,
-    COUNT(*) AS duplicate_count
-FROM gold.dim_customers
-GROUP BY customer_key
-HAVING COUNT(*) > 1;
+    SELECT 
+        customer_key,
+        COUNT(*) AS duplicate_count
+    FROM gold.dim_customers
+    GROUP BY customer_key
+    HAVING COUNT(*) > 1;
 
--- ====================================================================
--- Checking 'gold.product_key'
--- ====================================================================
+/*
+====================================================================
+Checking 'gold.product_key'
+====================================================================
+*/
+
 -- Check for Uniqueness of Product Key in gold.dim_products
 -- Expectation: No results 
-SELECT 
-    product_key,
-    COUNT(*) AS duplicate_count
-FROM gold.dim_products
-GROUP BY product_key
-HAVING COUNT(*) > 1;
+    SELECT 
+        product_key,
+        COUNT(*) AS duplicate_count
+    FROM gold.dim_products
+    GROUP BY product_key
+    HAVING COUNT(*) > 1;
 
--- ====================================================================
--- Checking 'gold.fact_sales'
--- ====================================================================
--- Check the data model connectivity between fact and dimensions
+/*
+====================================================================
+Checking 'gold.fact_sales'
+====================================================================
+*/
+
+-- Check the data model connectivity between fact and dimensions (FOREIGN KEY INTEGRITY)
 SELECT * 
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
